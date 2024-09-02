a26c1a91934f03f54d03e528c87c95f78795ff2b
Andy Lazcano <36870910+Alazca@users.noreply.github.com>
2024-09-02 01:28:23 -0700

    Add files via upload

--- Lab1/studentArray.cpp
+++ Lab1/studentArray.cpp
@@ -2,6 +2,24 @@
 
 using namespace std;
 
-studentArray::studentArray(){
-    
+studentArray::studentArray(){};
+studentArray::~studentArray(){};
+
+void studentArray::addStudent(const Student& newStudent){
+    int index = 0;
+    if (index < 0 || index >= size) {
+    throw out_of_range("Out of range Insertion!");
 }
+    if (size > MAX_CAPACITY) {
+    throw out_of_range("At capacity, can not insert");
+}
+    for (int i = size; i > index; i--) {
+    array[i] = array[i - 1];
+  }
+  size++;
+
+};
+
+void studentArray::dropStudent(const Student& droppedStudent){
+    int index = 0;
+};
--- Lab1/studentArray.h
+++ Lab1/studentArray.h
@@ -5,13 +5,19 @@
 using namespace std;
 
 class studentArray{
+private:
+  Student* array;
+  int size;
+  int MAX_CAPACITY = 100;
+public:
   studentArray();
   studentArray(const Student*);
   ~studentArray();
-  void addStudent(const Student&);
-  void dropStudent();
-  void updateStudent(int, const string&, const string&);
-  void selectionsort();
+  void addStudent(const Student&); // acts as insert
+  void dropStudent(Student&); // acts as remove
+  void updateStudent(int, const string&, const string&); // acts as replace at
+  void selectionsort(); // chosen sorting algo due to size of roster
+  void selectionsort(Student*); // overloaded operator in case array has to be sent over as parameter
 };
 
 #endif
