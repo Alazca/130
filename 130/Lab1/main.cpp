a474646fa473981c435f3870debe1e68c268f8db
Andy Lazcano <36870910+Alazca@users.noreply.github.com>
2024-09-01 21:11:41 -0700

    Add files via upload

---
+++ 130/Lab1/main.cpp
@@ -0,0 +1,6 @@
+#include "roster.h"
+#include <iostream>
+
+using namespace std;
+
+int main() { return 0; }
---
+++ 130/Lab1/roster.cpp
@@ -0,0 +1,19 @@
+#include "roster.h"
+
+Student::Student() {
+  firstName = "";
+  lastName = "";
+  id = 0;
+  status = "";
+}
+
+string Student::getFirst() const { return firstName; }
+
+string Student::getLast() const { return lastName; }
+int Student::getId() const { return id; }
+string Student::getStatus() const { return status; }
+
+void Student::setFirst(string &firstName) { this->firstName = firstName; }
+void Student::setLast(string &lastName) { this->lastName = lastName; }
+void Student::setId(int &id) { this->id = id; }
+void Student::setStatus(string &status) { this->status = status; }
